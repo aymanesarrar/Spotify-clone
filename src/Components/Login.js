dd7f48156dfa482bc6b9d52027133575a7d1a306
Aymane sarrar <sarrarayman@yahoo.fr>
2020-08-25 23:43:40 +0100

    setup

---
+++ src/Components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Login.css';
+function Login() {
+  return (
+    <div className="login">
+      {/* spotify logo */}
+      <h1>hh</h1>
+      {/* login with spotify button */}
+    </div>
+  )
+}
+
+export default Login
